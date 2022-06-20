838c1fee5929c2871cb802154d3b05fe3c16dceb
Maurice Schwarz <maurice.schwarz.dev@gmail.com>
2022-06-20 16:49:58 +0200

    feat: command nearly working

--- src/Controller/ContentModelController.php
+++ src/Controller/ContentModelController.php
@@ -3,14 +3,18 @@
 namespace App\Controller;
 
 use App\Service\EntityGenerator;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContentModelController extends AbstractController
 {
     #[Route('/content/model', name: 'app_content_model')]
-    public function index(EntityGenerator $generator): Response
+    public function index(EntityGenerator $generator, KernelInterface $kernel): Response
     {
         $entityName = 'Post';
         $tableName = 'post';
@@ -33,9 +37,32 @@ class ContentModelController extends AbstractController
 
         //create doctrine entity
 
+
         //create database table
 
         //execute database table
+        $application = new Application($kernel);
+        $application->setAutoExit(false);
+
+        $input = new ArrayInput([
+            'command' => 'make:entity',
+            // (optional) define the value of command arguments
+            // (optional) pass options to the command
+            '--regenerate',
+            '--overwrite',
+            'name' => 'App\Entity',
+        ]);
+
+        $input->setInteractive(false);
+        // You can use NullOutput() if you don't need the output
+        $output = new BufferedOutput();
+        $application->run($input, $output);
+
+        // return the output, don't use if you used NullOutput()
+        $content = $output->fetch();
+
+        // return new Response(""), if you used NullOutput()
+        return new Response($content);
 
         //return response
 
